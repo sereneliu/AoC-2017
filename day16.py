25a57047aaec1fd185b76d4c6cb521ba083c5089
Serene Liu <cloud9@sentanother.email>
2017-12-16 06:24:46 +0000

    Day 16: Permutation Promenade | Both Parts

--- day16.py
+++ day16.py
@@ -51,4 +51,29 @@ def dance_moves(programs, instructions):
             programs = partner(instruction[1:instruction.index('/')], instruction[instruction.index('/') + 1:], programs)
     return programs
 
-print dance_moves(puzzle_programs, puzzle_input)
+# print dance_moves(puzzle_programs, puzzle_input)
+
+# --- Part Two ---
+
+# Now that you're starting to get a feel for the dance moves, you turn your attention to the dance as a whole.
+
+# Keeping the positions they ended up in from their previous dance, the programs perform it again and again: including the first dance, a total of one billion (1000000000) times.
+
+# In the example above, their second dance would begin with the order baedc, and use the same dance moves:
+
+# s1, a spin of size 1: cbaed.
+# x3/4, swapping the last two programs: cbade.
+# pe/b, swapping programs e and b: ceadb.
+
+# In what order are the programs standing after their billion dances?
+
+def billion_dances(programs, instructions):
+    seen = []
+    for n in range(1000000000):
+        if programs not in seen:
+            seen.append(programs)
+            programs = dance_moves(programs, instructions)
+        else:
+            return seen[1000000000 % len(seen)]
+
+print billion_dances(puzzle_programs, puzzle_input)
