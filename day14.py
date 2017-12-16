18bad498cc3c025a7d430dabc98b75ab92dd39a3
Serene Liu <cloud9@sentanother.email>
2017-12-16 07:01:48 +0000

    Day 14: Disk Defragmentation | Both Parts

--- day14.py
+++ day14.py
@@ -27,19 +27,88 @@
 
 # Given your actual key string, how many squares are used?
 
-puzzle_input = '''wenycdww'''
+test_input = 'flqrgnkx'
+puzzle_input = 'wenycdww'
 
 from day10 import knot_hash
 
 def knot_hash_grid(puzzle_input):
     used_squares = 0
+    grid = []
     for n in range(128):
         binary_hash = []
         for num in knot_hash(puzzle_input + '-' + str(n)):
-            binary_hash.append('{0:b}'.format(num))
-        for bit in ''.join(binary_hash):
+            binary_hash.append('{0:b}'.format(num).zfill(8))
+        binary_hash = ''.join(binary_hash)
+        row = []
+        for bit in binary_hash:
             if bit == str(1):
+                bit = '#'
                 used_squares += 1
-    return used_squares
+            else:
+                bit = '.'
+            row.append(bit)
+        grid.append(row)
+#    print used_squares
+    return grid
 
-print knot_hash_grid(puzzle_input)
+# print knot_hash_grid(puzzle_input)
+
+# --- Part Two ---
+
+# Now, all the defragmenter needs to know is the number of regions. A region is a group of used squares that are all adjacent, not including diagonals. Every used square is in exactly one region: lone used squares form their own isolated regions, while several adjacent squares all count as a single region.
+
+# In the example above, the following nine regions are visible, each marked with a distinct digit:
+
+# 11.2.3..-->
+# .1.2.3.4   
+# ....5.6.   
+# 7.8.55.9   
+# .88.5...   
+# 88..5..8   
+# .8...8..   
+# 88.8.88.-->
+# |      |   
+# V      V   
+
+# Of particular interest is the region marked 8; while it does not appear contiguous in this small view, all of the squares marked 8 are connected when considering the whole 128x128 grid. In total, in this example, 1242 regions are present.
+
+# How many regions are present given your key string?
+
+connection_list = {}
+
+def find_connections(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            connection_list[str(i) + '-' + str(j)] = [grid[i][j]]
+            if grid[i][j] == '#':
+                if j != len(grid[i]) - 1 and grid[i][j + 1] == '#':
+                        connection_list[str(i) + '-' + str(j)].append(str(i) + '-' + str(j + 1))
+                if j != 0 and grid[i][j - 1] == '#':
+                        connection_list[str(i) + '-' + str(j)].append(str(i) + '-' + str(j - 1))
+                if i != len(grid) - 1 and grid[i + 1][j] == '#':
+                        connection_list[str(i) + '-' + str(j)].append(str(i + 1) + '-' + str(j))
+                if i != 0 and grid[i - 1][j] == '#':
+                        connection_list[str(i) + '-' + str(j)].append(str(i - 1) + '-' + str(j))
+
+find_connections(knot_hash_grid(puzzle_input))
+
+def find_squares(key, region_containing_key):
+    if key not in region_containing_key:
+        region_containing_key.add(key)
+        for connected in connection_list[key][1:]:
+            find_squares(connected, region_containing_key)
+    return region_containing_key
+
+def find_regions(some_list):
+    regions = []
+    regions_remaining = set(some_list.keys())
+    for key in connection_list.keys():
+        if key in regions_remaining:
+            region_key = find_squares(key, set())
+            if connection_list[key] != ['.']:
+                regions.append(region_key)
+            regions_remaining = regions_remaining.difference(region_key)
+    return len(regions)
+
+print find_regions(connection_list)
