6e1af7047049ec25d2d0c0d8fcff15b64f3473aa
Serene Liu <cloud9@sentanother.email>
2017-12-23 21:53:53 +0000

    Day 23: Coprocessor Conflagration | Part 2 - rewrote puzzle input as function

--- day23.py
+++ day23.py
@@ -25,12 +25,39 @@ def find_registers(instructions):
         if instruction[1].isalpha():
             register_dict[instruction[1]] = 0
 
-def read_instructions(instructions):
-    find_registers(instructions)
-    mul_instruction = 0
-    i = 0
-    num = 0
-    while i < len(instructions):
+# def read_instructions(instructions):
+#     find_registers(instructions)
+#     mul_instruction = 0
+#     i = 0
+#     num = 0
+#     while i < len(instructions):
+#         instruction = instructions[i][0]
+#         if instructions[i][1] not in register_dict.keys():
+#             register = int(instructions[i][1])
+#         else:
+#             register = instructions[i][1]
+#         if len(instructions[i]) > 2:
+#             if instructions[i][2] not in register_dict.keys():
+#                 num = int(instructions[i][2])
+#             else:
+#                 num = register_dict[instructions[i][2]]
+#         if instruction == 'set':
+#             register_dict[register] = num
+#         if instruction == 'sub':
+#             register_dict[register] -= num
+#         if instruction == 'mul':
+#             register_dict[register] *= num
+#             mul_instruction += 1
+#         if instruction == 'jnz' and register_dict.get(register, register) != 0:
+#             i += num
+#         else:
+#             i += 1
+#     return mul_instruction
+
+# print read_instructions(puzzle_input)
+
+def read_instructions(instructions, i, times_to_loop):
+    for l in xrange(times_to_loop):
         instruction = instructions[i][0]
         if instructions[i][1] not in register_dict.keys():
             register = int(instructions[i][1])
@@ -47,11 +74,60 @@ def read_instructions(instructions):
             register_dict[register] -= num
         if instruction == 'mul':
             register_dict[register] *= num
-            mul_instruction += 1
         if instruction == 'jnz' and register_dict.get(register, register) != 0:
             i += num
         else:
             i += 1
-    return mul_instruction
 
-print read_instructions(puzzle_input)
+def debug_mode(instructions):
+    find_registers(instructions)
+    register_dict['a'] = 1
+    read_instructions(instructions, 0, 10)
+    for b in xrange(register_dict['b'], register_dict['c'] + 1, 17):
+        register_dict['f'] = 1
+        for d in xrange(2, register_dict['b'] + 1):
+            for e in xrange(2, register_dict['b'] + 1):
+                if register_dict['d'] * register_dict['e'] == register_dict['b']:
+                    register_dict['f'] = 0
+        if register_dict['f'] == 0:
+            register_dict['h'] += 1
+    return register_dict['h']
+    
+print debug_mode(puzzle_input)
+
+# set b 57
+# set c b
+# jnz a 2
+# jnz 1 5
+# mul b 100
+# sub b -100000
+# set c b
+# sub c -17000
+#    set f 1
+#    set d 2
+#        set e 2
+#            set g d
+#            mul g e
+#            sub g b
+#            jnz g 2
+#            set f 0
+#            sub e -1
+#            set g e
+#            sub g b
+#            jnz g -8
+#        sub d -1
+#        set g d
+#        sub g b
+#        jnz g -13
+#    jnz f 2
+#    sub h -1
+#    set g b
+#    sub g c
+#    jnz g 2
+#    jnz 1 3
+#    sub b -17
+#    jnz 1 -23
+
+# assert register_dict['e'] != register_dict['b'], "loop: %s, %s" % (l, register_dict)
+# AssertionError: loop: 422796, {'a': 1, 'c': 122700, 'b': 105700, 'e': 52850, 'd': 2, 'g': 0, 'f': 1, 'h': 0}
+# AssertionError: loop: 845591, {'a': 1, 'c': 122700, 'b': 105700, 'e': 105700, 'd': 2, 'g': 105698, 'f': 0, 'h': 0}
