c405fa849fef211d94d96c4768d590379fd27880
Serene Liu <cloud9@sentanother.email>
2017-12-26 18:24:02 +0000

    Day 2: Corruption Checksum | Both Parts

--- day2.py
+++ day2.py
@@ -17,24 +17,8 @@
 
 # What is the checksum for the spreadsheet in your puzzle input?
 
-test_input = """104	240	147	246	123	175	372	71	116	230	260	118	202	270	277	292
-740	755	135	205	429	822	844	90	828	115	440	805	526	91	519	373
-1630	991	1471	1294	52	1566	50	1508	1367	1489	55	547	342	512	323	51
-1356	178	1705	119	1609	1409	245	292	1434	694	405	1692	247	193	1482	1407
-2235	3321	3647	212	1402	3711	3641	1287	2725	692	1235	3100	123	144	104	101
-1306	1224	1238	186	751	734	1204	1275	366	149	1114	166	1118	239	153	943
-132	1547	1564	512	2643	2376	2324	2159	1658	107	1604	145	2407	131	2073	1878
-1845	91	1662	108	92	1706	1815	1797	1728	1150	1576	83	97	547	1267	261
-78	558	419	435	565	107	638	173	93	580	338	52	633	256	377	73
-1143	3516	4205	3523	148	401	3996	3588	300	1117	2915	1649	135	134	182	267
-156	2760	1816	2442	2985	990	2598	1273	167	821	138	141	2761	2399	1330	1276
-3746	3979	2989	161	4554	156	3359	173	3319	192	3707	264	762	2672	4423	2924
-3098	4309	4971	5439	131	171	5544	595	154	571	4399	4294	160	6201	4329	5244
-728	249	1728	305	2407	239	691	2241	2545	1543	55	2303	1020	753	193	1638
-260	352	190	877	118	77	1065	1105	1085	1032	71	87	851	56	1161	667
-1763	464	182	1932	1209	640	545	931	1979	197	1774	174	2074	1800	939	161"""
-
-test_matrix = [[int(cell) for cell in row.split("\t")] for row in test_input.split("\n")]
+with open('day2.txt') as puzzle_file:
+    puzzle_input = [[int(cell) for cell in row.split("\t")] for row in puzzle_file.read().split("\n")]
 
 def max_min(matrix):
     diff = []
@@ -46,7 +30,7 @@ def max_min(matrix):
 def checksum(some_input):
     return sum(max_min(some_input))
 
-print checksum(test_matrix)
+print checksum(puzzle_input) # answer: 36766
 
 # --- Part Two ---
 
@@ -75,11 +59,11 @@ def divisible(matrix):
         for i in range(len(row)):
             for j in range(len(row)):
                 if i != j and row[j] % row[i] == 0:
-                    print row, "->", row[i], row[j], row[j] / row[i]
+                    # print row, "->", row[i], row[j], row[j] / row[i]
                     div.insert(len(div), row[j] / row[i])
     return div
 
 def checksum_div(matrix):
     return sum(divisible(matrix))
 
-print checksum_div(test_matrix)
+print checksum_div(puzzle_input) # answer: 261
